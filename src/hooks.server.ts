4f457b709dfa49beab5a58449ec2244f927c38a7
Donald Chatelain <djchatelain@gmail.com>
2023-11-08 12:43:13 -0700

    uses pg service connection vs localhost

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,11 @@
 // https://kit.svelte.dev/docs/hooks#server-hooks
 import type { Handle } from '@sveltejs/kit';
+import { PG_CONNECTION_STRING } from '$env/static/private';
 
 import postgres from 'postgres';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const sql = postgres('postgres://localhost:5432/dj');
+	const sql = postgres(PG_CONNECTION_STRING);
 
 	event.locals = {
 		sql: sql
